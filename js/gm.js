3e77e6ec2fc3a958cec3318a887d4755cf329380
Gonzalo Martin <gonzalo.mcruz@gmail.com>
2014-03-11 00:08:32 +0100

    Creating gm.js

---
+++ js/gm.js
@@ -0,0 +1,11 @@
+/*
+ * gm.js
+ */
+
+var home = function () {
+  if (window.location.pathname !== "gonzalomcruz.github.io/index.html") {
+    window.open ("index.html", '_self');
+  } else {
+    window.scrollTo(0, 0);
+  }
+};
