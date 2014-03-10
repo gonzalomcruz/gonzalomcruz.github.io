8defac19ea4bfc910cc48fd483f614f7508b2ce0
Gonzalo Martin <gonzalo.mcruz@gmail.com>
2014-03-11 00:12:46 +0100

    New new fix

--- js/gm.js
+++ js/gm.js
@@ -3,7 +3,7 @@
  */
 
 var home = function () {
-  if (window.location.pathname != "/index.html") {
+  if (window.location.pathname !== "/index.html") {
     window.open ("index.html", '_self');
   } else {
     window.scrollTo(0, 0);
